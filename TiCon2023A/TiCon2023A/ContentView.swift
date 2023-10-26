b9a02cf7dd5376eef502ccc4ea843513b94a86c2
mandeuk <mandeuk1014@gmail.com>
2023-10-26 14:02:12 +0900

    feat: add project files

---
+++ TiCon2023A/TiCon2023A/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TiCon2023A
+//
+//  Created by Inho Lee on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ TiCon2023A/TiCon2023A/TiCon2023AApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiCon2023AApp.swift
+//  TiCon2023A
+//
+//  Created by Inho Lee on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TiCon2023AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
