b34809b4a1da442417b86b76b43bb7930391aaf8
mandeuk <mandeuk1014@gmail.com>
2023-10-26 14:10:19 +0900

    feat: add root files

---
+++ TiCon2023A/TiCon2023A/View/Root/RootView.swift
@@ -0,0 +1,8 @@
+//
+//  RootView.swift
+//  TiCon2023A
+//
+//  Created by Inho Lee on 10/26/23.
+//
+
+import Foundation
---
+++ TiCon2023A/TiCon2023A/View/Root/ScreenFeature.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenFeature.swift
+//  TiCon2023A
+//
+//  Created by Inho Lee on 10/26/23.
+//
+
+import Foundation
